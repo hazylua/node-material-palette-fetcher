11777b6f4412ed411e750d216814c5548df77e51
hazylua <40061395+hazylua@users.noreply.github.com>
2022-03-11 23:46:24 -0300

    Utiliza .env

--- routes/colors.js
+++ routes/colors.js
@@ -28,4 +28,24 @@ router.get('/', function (req, res, next) {
   res.send(palette_labeled);
 });
 
+router.get('/:color', function (req, res, next) {
+  let palette = makePalette(req.params.color);
+  let labels = [
+    '50',
+    '100',
+    '200',
+    '300',
+    '400',
+    '500',
+    '600',
+    '700',
+    '800',
+    '900'
+  ];
+  palette_labeled = {};
+  palette.map((color, i) => (palette_labeled[labels[i]] = color));
+  console.log(req.query, palette_labeled);
+  res.send(palette_labeled);
+});
+
 module.exports = router;
